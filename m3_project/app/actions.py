4a15465213857e578a53fb8aa2624e0822508a51
Курбатов Владимир <warkb@yandex.ru>
2021-09-23 18:54:40 +0400

    m

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -1,11 +1,30 @@
-from objectpack.actions import ObjectPack
+from objectpack.actions import ObjectPack, BaseAction, ObjectSaveAction
 from objectpack.ui import ModelEditWindow
-
+from objectpack.observer.base import Observer, ObservableController
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
-from .ui import UserAddWindow
+from .ui import UserAddWindow, PermissionAddWindow
 
+
+
+obs =Observer()
+
+action_controller = ObservableController(obs, "/controller")
+
+@obs.subscribe
+class Listener(object):
+
+    # список регулярок, для сопоставления экшенам
+    listen = ['ObjectSaveAction']
+
+    def before(self, request, context, response):
+        print('===============================')
+        print('===============================')
+        print('===============================')
+        print('===============================')
+        print('===============================')
+        print('===============================')
 
 class UserPack(ObjectPack):
     model = User
@@ -25,9 +44,14 @@ class GroupPack(ObjectPack):
     can_delete = True
     add_window = edit_window = ModelEditWindow.fabricate(model=Group)
 
+
 class PermissionPack(ObjectPack):
     model = Permission
     parents = ['content_type']
     add_to_menu = True
     can_delete = True
-    add_window = edit_window = ModelEditWindow.fabricate(model=Permission)
+    add_window = edit_window = PermissionAddWindow 
+    def __init__(self):
+        super(PermissionPack, self).__init__()
+
+
--- m3_project/app/ui.py
+++ m3_project/app/ui.py
@@ -1,6 +1,6 @@
 from objectpack.ui import BaseEditWindow, make_combo_box, model_fields_to_controls
 from m3_ext.ui import all_components as ext
-
+from objectpack.ui import ModelEditWindow
 from django.contrib.auth.models import User, Permission
 
 
@@ -95,42 +95,42 @@ class UserAddWindow(BaseEditWindow):
 
 
 
-# class PermissionAddWindow(BaseEditWindow):
-#     def _init_components(self):
-#         super()._init_components()
+class PermissionAddWindow(BaseEditWindow):
+    def _init_components(self):
 
-#         self.field__name = ext.ExtStringField(
-#             label=u'name',
-#             name='name',
-#             anchor='100%')
+        super()._init_components()
+        self.field__name = ext.ExtStringField(
+            label=u'name',
+            name='name',
+            anchor='100%')
 
-#         self.field__codename = ext.ExtStringField(
-#             label=u'codename',
-#             name='codename',
-#             anchor='100%')
+        self.field__codename = ext.ExtStringField(
+            label=u'codename',
+            name='codename',
+            anchor='100%')
 
-#         self.field__content_type = model_fields_to_controls(
-#             model=Permission,
-#             window=)
+        self.field__content_type = ext.ExtDictSelectField(
+            label=u'content_type',
+            name=u'content_type')
 
-        
 
-#     def _do_layout(self):
-#         """
-#         Здесь размещаем компоненты в окне
-#         """
-#         super()._do_layout()
-#         self.form.items.extend((
-#             self.field__name,
-#             self.field__content_type,
-#             self.field__codename
-#         ))
+    def _do_layout(self):
+        """
+        Здесь размещаем компоненты в окне
+        """
+        super()._do_layout()
+        self.form.items.extend((
+            self.field__name,
+            self.field__content_type,
+            self.field__codename
+        ))
 
-#     def set_params(self, params):
-#         """
-#         Установка параметров окна
+    def set_params(self, params):
+        """
+        Установка параметров окна
 
-#         :params: Словарь с параметрами, передается из пака
-#         """
-#         super().set_params(params)
-#         self.height = 'auto'
+        :params: Словарь с параметрами, передается из пака
+        """
+        super().set_params(params)
+        self.height = 'auto'
+        self.field__content_type.pack = 'app.actions.ContentTypePack'
